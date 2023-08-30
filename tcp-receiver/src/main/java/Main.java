bb8d68bdf3bdd3ce641abe0cca9cd18c7b5ba0f6
SciampiJacopo <91743869+SciampiJacopo@users.noreply.github.com>
2023-08-30 14:52:48 +0200

    Fix Java

--- tcp-receiver/src/main/java/Main.java
+++ tcp-receiver/src/main/java/Main.java
@@ -14,6 +14,7 @@ import org.json.JSONObject;
 import org.json.JSONException;
 import org.json.JSONArray;
 
+// mvn clean package -> build
 public class Main {
     private static Set<String> clientSN = new HashSet<>();
     private static Set<KeyValuePair> deviceSnAndTimeZone = new HashSet<>();
