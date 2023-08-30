06f65652aadbb076577a4788b441d4f25bebf816
SciampiJacopo <91743869+SciampiJacopo@users.noreply.github.com>
2023-08-30 14:47:28 +0200

    WIP java

--- tcp-receiver/src/main/java/Main.java
+++ tcp-receiver/src/main/java/Main.java
@@ -14,41 +14,6 @@ import org.json.JSONObject;
 import org.json.JSONException;
 import org.json.JSONArray;
 
-class KeyValuePair {
-    private String key;
-    private String value;
-
-    public KeyValuePair(String key, String value) {
-        this.key = key;
-        this.value = value;
-    }
-
-    public String getKey() {
-        return key;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(key, value);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        KeyValuePair other = (KeyValuePair) obj;
-        return key == other.key && Objects.equals(value, other.value);
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
-}
-
 public class Main {
     private static Set<String> clientSN = new HashSet<>();
     private static Set<KeyValuePair> deviceSnAndTimeZone = new HashSet<>();
