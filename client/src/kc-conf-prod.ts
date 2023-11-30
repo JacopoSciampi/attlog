399cb045397796d54c5dcd66a25e05337b4f54e2
Jeko <sciampi.jacopo@gmail.com>
2023-11-30 17:44:44 +0100

    Fix: kc prod url

--- client/src/kc-conf-prod.ts
+++ client/src/kc-conf-prod.ts
@@ -1,7 +1,7 @@
 import { AuthConfig } from "angular-oauth2-oidc";
 
 export const KC_CONF: AuthConfig = {
-    issuer: "http://192.168.0.161/auth/realms/timbrature",
+    issuer: "http://192.168.0.161:88/auth/realms/timbrature",
     redirectUri: window.origin,
     clientId: "client-app",
     responseType: "code",
@@ -10,5 +10,5 @@ export const KC_CONF: AuthConfig = {
     timeoutFactor: 0.75,
     clearHashAfterLogin: true,
     requireHttps: false,
-    logoutUrl: "http://192.168.0.161/auth/realms/timbrature/protocol/openid-connect/logout"
+    logoutUrl: "http://192.168.0.161:88/auth/realms/timbrature/protocol/openid-connect/logout"
 }
